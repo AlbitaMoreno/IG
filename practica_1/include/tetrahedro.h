e5cae767625fdf6ce62dd4073f88512ad7fccfba
Alba <albamo@correo.ugr.es>
2019-09-29 18:28:24 +0200

    piramide y tetrahedro añadidos

---
+++ practica_1/include/tetrahedro.h
@@ -0,0 +1,11 @@
+#ifndef TETRAHEDRO_H
+#define TETRAHEDRO_H
+
+#include <o3d.h>
+
+class Tetrahedro : public O3D {
+public:
+    Tetrahedro();
+};
+
+#endif // TETRAHEDRO_H
---
+++ practica_1/src/models/tetrahedro.cc
@@ -0,0 +1,15 @@
+#include "include/tetrahedro.h"
+
+Tetrahedro :: Tetrahedro()
+{
+    //h = sqrt(6)/3
+    vertices.push_back(_vertex3f(0.0,0.43, 0.0));   //cúspide
+    vertices.push_back(_vertex3f(0.5, -0.43,-0.37));
+    vertices.push_back(_vertex3f(-0.5,-0.43,-0.37));
+    vertices.push_back(_vertex3f(0.0,-0.43, 0.5));
+
+    triangulos.push_back(_vertex3i(0,1,2));    //e0
+    triangulos.push_back(_vertex3i(0,2,3));    //e1
+    triangulos.push_back(_vertex3i(0,3,1));    //e2
+    triangulos.push_back(_vertex3i(2,1,3));    //e4
+}
